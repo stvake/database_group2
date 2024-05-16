c352ea9c142ca398b8446b761d34320effbc90c3
Zarie1 <160772594+Zarie1@users.noreply.github.com>
2024-05-16 12:47:52 +0300

    Виправлення та insert в sql

--- src/sql/Script.sql
+++ src/sql/Script.sql
@@ -74,19 +74,18 @@ DROP TABLE IF EXISTS `mydb`.`Access` ;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Access` (
   `Access.id` INT NOT NULL,
-  `Datafile_Datafile.id` INT NOT NULL,
   `User_User.id` INT NOT NULL,
-  `Datafile_Datafile.id1` INT NOT NULL,
-  PRIMARY KEY (`Access.id`, `Datafile_Datafile.id`),
+  `Datafile_Datafile.id` INT NOT NULL,
+  PRIMARY KEY (`Access.id`),
   INDEX `fk_Access_User1_idx` (`User_User.id` ASC) VISIBLE,
-  INDEX `fk_Access_Datafile1_idx` (`Datafile_Datafile.id1` ASC) VISIBLE,
+  INDEX `fk_Access_Datafile1_idx` (`Datafile_Datafile.id` ASC) VISIBLE,
   CONSTRAINT `fk_Access_User1`
     FOREIGN KEY (`User_User.id`)
     REFERENCES `mydb`.`User` (`User.id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Access_Datafile1`
-    FOREIGN KEY (`Datafile_Datafile.id1`)
+    FOREIGN KEY (`Datafile_Datafile.id`)
     REFERENCES `mydb`.`Datafile` (`Datafile.id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -104,18 +103,17 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Request` (
   `Request.message` VARCHAR(45) NULL,
   `User_User.id` INT NOT NULL,
   `Access_Access.id` INT NOT NULL,
-  `Access_Datafile_Datafile.id` INT NOT NULL,
-  PRIMARY KEY (`Request.id`, `User_User.id`, `Access_Access.id`, `Access_Datafile_Datafile.id`),
+  PRIMARY KEY (`Request.id`, `User_User.id`, `Access_Access.id`),
   INDEX `fk_Request_User1_idx` (`User_User.id` ASC) VISIBLE,
-  INDEX `fk_Request_Access1_idx` (`Access_Access.id` ASC, `Access_Datafile_Datafile.id` ASC) VISIBLE,
+  INDEX `fk_Request_Access1_idx` (`Access_Access.id` ASC) VISIBLE,
   CONSTRAINT `fk_Request_User1`
     FOREIGN KEY (`User_User.id`)
     REFERENCES `mydb`.`User` (`User.id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Request_Access1`
-    FOREIGN KEY (`Access_Access.id` , `Access_Datafile_Datafile.id`)
-    REFERENCES `mydb`.`Access` (`Access.id` , `Datafile_Datafile.id`)
+    FOREIGN KEY (`Access_Access.id`)
+    REFERENCES `mydb`.`Access` (`Access.id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -193,3 +191,119 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `mydb`.`Role`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `mydb`;
+INSERT INTO `mydb`.`Role` (`Role.id`, `Role.name`) VALUES (1, 'sadmin');
+INSERT INTO `mydb`.`Role` (`Role.id`, `Role.name`) VALUES (2, 'developer');
+INSERT INTO `mydb`.`Role` (`Role.id`, `Role.name`) VALUES (3, 'moderator');
+INSERT INTO `mydb`.`Role` (`Role.id`, `Role.name`) VALUES (4, 'user');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `mydb`.`User`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `mydb`;
+INSERT INTO `mydb`.`User` (`User.id`, `User.username`, `User.email`, `User.password`, `User.firstname`, `User.lastname`, `Usercol`, `Role_Role.id`) VALUES (1, 'user1', 'user1@gmal.com', 'abcdfg', 'Guy', 'Ritchie', 'a', 1);
+INSERT INTO `mydb`.`User` (`User.id`, `User.username`, `User.email`, `User.password`, `User.firstname`, `User.lastname`, `Usercol`, `Role_Role.id`) VALUES (2, 'user2', 'user2@gmal.com', 'abcdfo', 'Big', 'Lebovski', 'b', 3);
+INSERT INTO `mydb`.`User` (`User.id`, `User.username`, `User.email`, `User.password`, `User.firstname`, `User.lastname`, `Usercol`, `Role_Role.id`) VALUES (3, 'user3', 'user3@gmal.com', 'abcdfp', 'Quentin', 'Tarantino', 'c', 4);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `mydb`.`Datafile`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `mydb`;
+INSERT INTO `mydb`.`Datafile` (`Datafile.id`, `Datafile.name`, `Datafile.content`, `Datafile.description`, `Datafile.format`, `Datafile.date`) VALUES (1, 'Schema', 'photo', 'schema of relations', 'png', '2024-5-12');
+INSERT INTO `mydb`.`Datafile` (`Datafile.id`, `Datafile.name`, `Datafile.content`, `Datafile.description`, `Datafile.format`, `Datafile.date`) VALUES (2, 'Docs', 'information', 'information about team', 'word', '2024-5-13');
+INSERT INTO `mydb`.`Datafile` (`Datafile.id`, `Datafile.name`, `Datafile.content`, `Datafile.description`, `Datafile.format`, `Datafile.date`) VALUES (3, 'Docs', 'information', 'development information', 'word', '2024-5-14');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `mydb`.`Access`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `mydb`;
+INSERT INTO `mydb`.`Access` (`Access.id`, `User_User.id`, `Datafile_Datafile.id`) VALUES (1, 1, 1);
+INSERT INTO `mydb`.`Access` (`Access.id`, `User_User.id`, `Datafile_Datafile.id`) VALUES (2, 2, 2);
+INSERT INTO `mydb`.`Access` (`Access.id`, `User_User.id`, `Datafile_Datafile.id`) VALUES (3, 3, 3);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `mydb`.`Request`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `mydb`;
+INSERT INTO `mydb`.`Request` (`Request.id`, `Request.type`, `Request.message`, `User_User.id`, `Access_Access.id`) VALUES (1, 'login', 'You are logined', 1, 1);
+INSERT INTO `mydb`.`Request` (`Request.id`, `Request.type`, `Request.message`, `User_User.id`, `Access_Access.id`) VALUES (2, 'login', 'You are logined', 2, 2);
+INSERT INTO `mydb`.`Request` (`Request.id`, `Request.type`, `Request.message`, `User_User.id`, `Access_Access.id`) VALUES (3, 'registration', 'You are registered', 3, 3);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `mydb`.`Tag`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `mydb`;
+INSERT INTO `mydb`.`Tag` (`Tag.name`) VALUES ('abc');
+INSERT INTO `mydb`.`Tag` (`Tag.name`) VALUES ('abd');
+INSERT INTO `mydb`.`Tag` (`Tag.name`) VALUES ('abh');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `mydb`.`Datafile_tag`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `mydb`;
+INSERT INTO `mydb`.`Datafile_tag` (`Datafile_Datafile.id`, `Tag_Tag.name`) VALUES (1, 'abc');
+INSERT INTO `mydb`.`Datafile_tag` (`Datafile_Datafile.id`, `Tag_Tag.name`) VALUES (2, 'abd');
+INSERT INTO `mydb`.`Datafile_tag` (`Datafile_Datafile.id`, `Tag_Tag.name`) VALUES (3, 'abh');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `mydb`.`Permission`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `mydb`;
+INSERT INTO `mydb`.`Permission` (`Permission.id`, `Permission.name`) VALUES (1, 'Create');
+INSERT INTO `mydb`.`Permission` (`Permission.id`, `Permission.name`) VALUES (2, 'Edit');
+INSERT INTO `mydb`.`Permission` (`Permission.id`, `Permission.name`) VALUES (3, 'Delete');
+INSERT INTO `mydb`.`Permission` (`Permission.id`, `Permission.name`) VALUES (4, 'Execute');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `mydb`.`Grant_ Permission`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `mydb`;
+INSERT INTO `mydb`.`Grant_ Permission` (`Permission_Permission.id`, `Role_Role.id`) VALUES (1, 1);
+INSERT INTO `mydb`.`Grant_ Permission` (`Permission_Permission.id`, `Role_Role.id`) VALUES (1, 2);
+INSERT INTO `mydb`.`Grant_ Permission` (`Permission_Permission.id`, `Role_Role.id`) VALUES (1, 3);
+INSERT INTO `mydb`.`Grant_ Permission` (`Permission_Permission.id`, `Role_Role.id`) VALUES (2, 1);
+INSERT INTO `mydb`.`Grant_ Permission` (`Permission_Permission.id`, `Role_Role.id`) VALUES (2, 2);
+INSERT INTO `mydb`.`Grant_ Permission` (`Permission_Permission.id`, `Role_Role.id`) VALUES (2, 3);
+INSERT INTO `mydb`.`Grant_ Permission` (`Permission_Permission.id`, `Role_Role.id`) VALUES (2, 4);
+INSERT INTO `mydb`.`Grant_ Permission` (`Permission_Permission.id`, `Role_Role.id`) VALUES (3, 1);
+INSERT INTO `mydb`.`Grant_ Permission` (`Permission_Permission.id`, `Role_Role.id`) VALUES (3, 3);
+
+COMMIT;
+
